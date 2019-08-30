44a8c7212f449e44b296c058db186b61ee00789b
Nan <ouchxp.me@gmail.com>
2019-08-30 16:23:36 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# GraphQL Server Example
+
+A minimalistic GraphQL server example build by following this document
+[https://www.apollographql.com/docs/apollo-server/getting-started/]
---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express'
+import typeDefs from './graphql/schema'
+import resolvers from './graphql/resolver'
+
+import http from 'http'
+const config = { port: 4000, hostname: 'localhost' }
+const apollo = new ApolloServer({ typeDefs, resolvers })
+
+const app = express();
+apollo.applyMiddleware({ app });
+let server = http.createServer(app);
+
+// Add subscription support
+apollo.installSubscriptionHandlers(server)
+
+server.listen({ port: config.port }, () =>
+  console.log(
+    '🚀 Server ready at',
+    `http://${config.hostname}:${config.port}${apollo.graphqlPath}`
+  )
+)
